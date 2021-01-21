8c3c7fe748345eb773c9dd082d9e76ddeca32461
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2021-01-21 11:18:46 -0600

    Update 2app_engine.sh

--- 2app_engine.sh
+++ 2app_engine.sh
@@ -3,3 +3,9 @@ gcloud projects create hugosgcppractice
 
 # set the project as the default project
 gcloud config set project hugosgcppractice
+
+# clonning the dummy app 
+git clone https://github.com/GoogleCloudPlatform/appengine-guestbook-python.git
+cd appengine-guestbook-python/
+ls -lha
+cat app.yaml
